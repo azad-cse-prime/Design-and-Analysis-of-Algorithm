a8c7f3489f1a43ea51421162ac38cb1ec123d617
Azad <azadcse14716@gmail.com>
2025-08-26 03:14:01 +0600

    Insertion Sort

---
+++ InsertionSort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void insertionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+       while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
+int main() {
+    int arr[] = {8, 9, 7, 5, 12, 4, 13, 2, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
